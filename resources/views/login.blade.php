aa58f3555eb76698c4542b6c3c1c03c535b88ae1
Victor <victor.mussel@hotmail.com>
2019-05-13 10:48:06 -0300

    Alteração Title

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,7 +8,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 	
-		<title>Sistema de Gestão de Frota</title>
+		<title>Sistema de Gestão de Consultas</title>
 	
 		<!--     Fonts and icons     -->
 		<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons" rel="stylesheet">
